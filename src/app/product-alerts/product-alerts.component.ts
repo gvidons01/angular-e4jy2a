66b138f83a2813bd5b517778df17b1a365f35717
gvidons01 <99792210+gvidons01@users.noreply.github.com>
2022-09-13 13:40:59 +0300

    2 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -7,6 +7,7 @@ import { Product } from '../products';
   styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent {
+  
   @Input() product: Product | undefined;
   @Output() notify = new EventEmitter();
 }
